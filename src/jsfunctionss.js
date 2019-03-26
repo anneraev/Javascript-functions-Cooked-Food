87b8d58966bb483d00fd586057cf30c4809bb051
anneraev <47757104+anneraev@users.noreply.github.com>
2019-03-26 17:28:17 -0500

    removed from other practices

---
+++ src/jsfunctionss.js
@@ -0,0 +1,48 @@
+const hamburger = {
+    name: 'Hamburger',
+    type: 'beef',
+    cooked: false,
+}
+const zucchini = {
+    name: 'Zucchini',
+    type: 'vegetable',
+    cooked: false,
+}
+const chickenBreast = {
+    name: 'Chicken Breast',
+    type: 'chicken',
+    cooked: false,
+}
+const corn = {
+    name: 'Corn',
+    type: 'vegetable',
+    cooked: false,
+}
+const steak = {
+    name: 'Steak',
+    type: 'beef',
+    cooked: false,
+}
+
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill(currentObject) {
+    // Modify the food so that it is cooked
+    if (currentObject.cooked === false) {
+        currentObject.cooked = true;
+    };
+    // Put the cooked food into the appropriate array
+    if (currentObject.cooked) {
+        cookedFood.push(currentObject);
+    };
+};
+
+for (var i = 0; i < foods.length; i += 1) {
+    grill(foods[i])
+};
+
+console.log(cookedFood);
